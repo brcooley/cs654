6a03bffdd4efa612a11ca918372e03ddf54888c9
brc <brett@cooleyweb.org>
2012-12-14 00:32:02 -0500

    Adding standardized merge_mpi / merge_omp, WIP merge_cuda

---
+++ mergesort/merge_cuda.cu
@@ -0,0 +1,151 @@
+/*
+ *	CS420 Homework 2 - Brett Cooley (brcooley@email.wm.edu)
+ *
+ *	Changed sumseq program to implement mergesort on a given subset of an array.  Uses
+ *		pthread_barrier_t to make sure all threads are in sync, and after each barrier
+ *		1/2 of the threads exit, the other half merging their data and the date of the
+ *		closest thread to them which exited.  Barriers are all initialized prior to
+ *		any thread creation, and just used in order.
+ *
+ *	Threads/Array size 		1024 			1048576			8388608			16777216
+ *		1 					.000/.000/.000	.313/.313/.312	2.87/2.87/2.87	 5.97/ 5.97/ 5.97
+ *		2 					.000/.000/.000	.334/.334/.334	3.04/3.04/3.04	 6.32/ 6.33/ 6.33
+ *		4 					.000/.000/.000	.352/.353/.354	3.42/3.42/3.42	 7.12/ 7.12/ 7.12
+ *		8					.001/.001/.001	.423/.452/.490	4.24/4.25/4.29	 8.85/ 8.87/ 8.90
+ *		16					.001/.001/.001	.459/.494/.523	5.02/5.05/5.10	10.47/10.50/10.54
+ *		32					.002/.002/.002	.506/.541/.635	5.75/5.80/5.90	12.02/12.07/12.15
+ *		64					.003/.003/.003	.571/.598/.622	6.36/6.43/6.53	13.49/13.54/13.59
+ *		128					.006/.006/.006	.575/.626/.677	6.89/6.96/7.03	14.77/14.82/14.87
+ *		256					.011/.011/.012	.619/.650/.733	7.28/7.56/7.75	15.84/15.93/16.06
+ *		512					.023/.024/.025	.665/.704/.749	8.03/8.09/8.12	17.12/17.35/17.61
+ */
+
+#include <cuda.h>
+#include <math.h>
+#include <fenv.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+__global__
+void mergesort(int *a);
+
+double getTime();
+void handleErrors(cudaError_t);
+
+
+/* Prototypes. */
+void *threadMain(void *arg);
+int merge(int,long,long,long);
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) { printf("usage: ./merge_cuda DEVICE ARRAY_SIZE NUM_THREADS\n"); exit(1); }
+
+	int *a, *dev_a, *dev_b;
+	double totalTime, startTime;
+
+	totalTime = 0;
+	int deviceNum = atoi(argv[1]);
+	int arraySize = atoi(argv[2]);
+	int numThreads = atoi(argv[3]);
+
+	char x = ((cudaSetDevice(deviceNum))== cudaSuccess)? 'Y' : 'N';
+	
+	/* Initialize the sequence. */
+	srand(654);
+	a = malloc(sizeof(int) * arraySize);
+	for(int i = 0; i < arraySize; i++) {
+		a[i] = ((int) random() / 100000.0);
+	}
+
+	cudaMalloc((void **)&dev_a, sizeof(int) * arraySize);
+	// cudaMalloc((void **)&dev_b, sizeof(int) * arraySize);
+	cudaMemcpy(dev_a, &a, sizeof(int) * arraySize, cudaMemcpyHostToDevice);
+	// cudaMemcpy(dev_b, &b, sizeof(int) * NUM_VALUES, cudaMemcpyHostToDevice);
+
+	// numThreads can range from 1..7 => {1,4,16,64,256,512,1024}
+	if (numThreads <= 2) {
+		dim3 blocks((int) pow(4,(numThreads-1), 1, 1);
+		dim3 threads(1, 1, 1);
+	} else if (numThreads <= 4) {
+		dim3 blocks(4*(numThreads-2), 4*(numThreads-2), 1);
+		dim3 threads(1, 1, 1);
+	} else {
+		dim3 blocks(16, 16, 1);
+		dim3 threads(1*((int) pow(2,numThreads - 5)), 1, 1);
+	}
+
+	startTime = getTime();
+	mergesort<<<blocks,threads>>>(dev_a);
+	cudaThreadSynchronize();
+	totalTime += getTime() - startTime;
+	handleErrors(cudaGetLastError());
+
+	cudaFree(dev_a);
+	printf("%.5f\n", totalTime);
+
+	/*for(int i=0; i<arraySize; i++)
+		printf("%f\n", a[outputArray][i]);
+	printf("\n");*/
+
+	return 0;
+}
+
+
+__global__
+void mergesort(int *a) {
+	int blockId = blockIdx.x * gridDim.y + blockIdx.y;
+	int threadId = threadIdx.x * blockDim.y + threadIdx.y;
+	int id = blockId * (blockDim.x * blockDim.y) + threadId;
+
+	int start =  * id;
+}
+
+
+void *threadMain(void *arg) {
+	long start = baseSize * threadId;
+	long subSize = baseSize;
+	int target = 1;
+
+	for (int j=0; j <= numRounds; j++) {
+		for (int width=1; width < subSize; width *= 2) {
+			for (int i=start; i<(start + subSize); i += 2 * width) {
+				merge(target, i, i+width, i+(2*width));
+			}
+			target = 1 - target;
+		}
+
+		pthread_barrier_wait(&sync[j]);
+		if (threadId == 0 || threadId % ((long) pow(2,j+1)) == 0) { subSize *= 2; }
+		else { return (void *) 0; }
+	}
+	if (threadId == 0) { outputArray = 1-target; }
+}
+
+
+int merge(int target, long left, long right, long end) {
+	long lEnd = right;
+	for (int i=left; i<end; i++) {
+		if (left < lEnd && (right >= end || values[1-target][left] <= values[1-target][right])) {
+			values[target][i] = values[1-target][left];
+			left++;
+		} else {
+			values[target][i] = values[1-target][right];
+			right++;
+		}
+	}
+	return 0;
+}
+
+
+double getTime() {
+	timeval thetime;
+	gettimeofday( &thetime, 0 );
+	return thetime.tv_sec + thetime.tv_usec / 1000000.0;
+}
+
+
+void handleErrors(cudaError_t err) {
+	if (err != cudaSuccess) {
+		printf("Kernel: %s\n",cudaGetErrorString(err));
+	}
+}
--- mergesort/merge_omp.cc
+++ mergesort/merge_omp.cc
@@ -5,96 +5,94 @@
 #include <stdlib.h>
 using namespace std;
 
-vector<long> merge(const vector<long>& left, const vector<long>& right)
-{
-    vector<long> result;
-    unsigned left_it = 0, right_it = 0;
+vector<long> merge(const vector<long>& left, const vector<long>& right) {
+	vector<long> result;
+	unsigned left_it = 0, right_it = 0;
 
-    while(left_it < left.size() && right_it < right.size())
-    {
-        if(left[left_it] < right[right_it])
-        {
-            result.push_back(left[left_it]);
-            left_it++;
-        }
-        else					
-        {
-            result.push_back(right[right_it]);
-            right_it++;
-        }
-    }
+	while(left_it < left.size() && right_it < right.size())	{
+		if(left[left_it] < right[right_it]) {
+			result.push_back(left[left_it]);
+			left_it++;
+		}
+		else {
+			result.push_back(right[right_it]);
+			right_it++;
+		}
+	}
 
-    // Push the remaining data from both vectors onto the resultant
-    while(left_it < left.size())
-    {
-        result.push_back(left[left_it]);
-        left_it++;
-    }
+	// Push the remaining data from both vectors onto the resultant
+	while(left_it < left.size()) {
+		result.push_back(left[left_it]);
+		left_it++;
+	}
 
-    while(right_it < right.size())
-    {
-        result.push_back(right[right_it]);
-        right_it++;
-    }
+	while(right_it < right.size()) {
+		result.push_back(right[right_it]);
+		right_it++;
+	}
 
-    return result;
+	return result;
 }
 
-vector<long> mergesort(vector<long>& vec, int threads)
-{
-    // Termination condition: List is completely sorted if it
-    // only contains a single element.
-    if(vec.size() == 1)
-    {
-        return vec;
-    }
+vector<long> mergesort(vector<long>& vec, int threads) {
+	// Termination condition: List is completely sorted if it
+	// only contains a single element.
+	if(vec.size() == 1)	{
+		return vec;
+	}
 
-    // Determine the location of the middle element in the vector
-    std::vector<long>::iterator middle = vec.begin() + (vec.size() / 2);
+	// Determine the location of the middle element in the vector
+	std::vector<long>::iterator middle = vec.begin() + (vec.size() / 2);
 
-    vector<long> left(vec.begin(), middle);
-    vector<long> right(middle, vec.end());
+	vector<long> left(vec.begin(), middle);
+	vector<long> right(middle, vec.end());
 
-    // Perform a merge sort on the two smaller vectors
+	// Perform a merge sort on the two smaller vectors
+	if (threads > 1) {
+		#pragma omp parallel sections
+		{
+			#pragma omp section
+			{
+				left = mergesort(left, threads/2);
+			}
+			#pragma omp section
+			{
+				right = mergesort(right, threads - threads/2);
+			}
+	  	}
+	}
+	else {
+	  left = mergesort(left, 1);
+	  right = mergesort(right, 1);
+	}
 
-    if (threads > 1)
-    {
-      #pragma omp parallel sections
-      {
-        #pragma omp section
-        {
-          left = mergesort(left, threads/2);
-        }
-        #pragma omp section
-        {
-          right = mergesort(right, threads - threads/2);
-        }
-      }
-    }
-    else
-    {
-      left = mergesort(left, 1);
-      right = mergesort(right, 1);
-    }
-
-    return merge(left, right);
+	return merge(left, right);
 }
 
-int main(int argc, char** argv)
-{
-  int numThreads;
-  if(argc == 1)  {numThreads = 8;  }
-  else{ numThreads = atoi(argv[1]); }
-  time_t t1;
-  time_t t2;
-  vector<long> v(10000000);
-  for (long i=0; i<10000000; ++i)
-    v[i] = (i * i) % 10000000;
-  omp_set_num_threads(numThreads);
-  t1 = time(NULL);
-  v = mergesort(v, numThreads);
-  t2 = time(NULL);
-  //for (long i=0; i<1000000; ++i)
-  //  cout << v[i] << "\n";
-  cout << t2-t1<< "\n";
+int main(int argc, char** argv) {
+	if (argc != 3) { printf("usage: ./merge_omp ARRAY_SIZE NUM_THREADS\n"); exit(1); }
+
+	int arraySize, numThreads;
+	double totalTime, startTime;
+
+	totalTime = 0;
+	arraySize = atoi(argv[1]);
+	numThreads = atoi(argv[2]);
+
+	vector<int> v(arraySize);
+	for (int i = 0; i < arraySize; ++i) {
+		v[i] = ((int) random() / 100000.0);
+	}
+
+	omp_set_num_threads(numThreads);
+	startTime = getTime();
+	v = mergesort(v, numThreads);
+	totalTime = getTime() - startTime;
+
+	//for (long i=0; i<1000000; ++i)
+	//  cout << v[i] << "\n";
+	// cout << t2-t1<< "\n";
+	
+	printf("%.5f\n", totalTime);
+	return 0;
 }
