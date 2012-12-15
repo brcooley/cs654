9bca9746a4745ccdf7516294290d73f46fe18e23
brc <brett@cooleyweb.org>
2012-12-15 11:01:57 -0500

    Changes to data compiler

--- compile_data.py
+++ compile_data.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 def stats(raw):
 	return (min(raw), sum(raw)/len(raw), max(raw), sd(raw))
@@ -6,6 +7,9 @@ def stats(raw):
 def sd(raw):
 	mean = sum(raw)/len(raw)
 	return (sum((x - mean) ** 2 for x in raw)/len(raw)) ** (.5)
+
+def new(cur, old):
+	return cur[0] != old[0] or cur[1] != old[1]
 
 def main():
 
@@ -16,16 +20,24 @@ def main():
 
 			v = 0
 			k = 0
+			last = (0, 0, 0)
 
 			for line in f:
 				try:
 					v = float(line)
 				except ValueError:
-					k = re.findall(r'[0-9]+', line)
-					if v != 0:
-						data[k] = v
+					k = tuple(re.findall(r'[0-9]+', line))[:-1]
+					if new(k, last):
+						data[k] = []
+						last = k
+					else:
+						data[k].append(v)
 
-		data_wrapped = { 'problem':raw_data.rsplit('_'), 'data':data }
+		for key in data.keys():
+			data[key] = stats(data[key])
+
+
+		data_wrapped = { 'problem': raw_data.rsplit('_'), 'data': data }
 
 		with open('final_results.dat', 'w+') as f_out: 
 			print(json.dumps(data_wrapped), file=f_out)
